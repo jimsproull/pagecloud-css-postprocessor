d1a76a2e7990cfd87067ed7a352d39e4504e236c
Jim Sproull <jim@pagecloud.com>
2020-12-09 07:57:54 -0500

    testing vars -> vars

--- index.js
+++ index.js
@@ -5,27 +5,28 @@ const htmlSource = `<html>
         --pc-color-1: rgba(0,1,2,.3);
         --pc-color-2: red;
     }
+    :root {
+        --my-custom-color: var(--pc-color-2);
+    }
 </style>
 <style>
     .foo {
         color: var(--pc-color-1);
         background-color: var(--pc-color-2);
+        border-color: var(--my-custom-color);
     }
 
     ::placeholder {
         color: gray;
         }
 
-        .image {
-        background-image: url(image@1x.png);
-        }
         @media (min-resolution: 2dppx) {
         .image {
             background-image: url(image@2x.png);
         }
         }
 </style>
-<div class="foo">
+<div class="foo" style="color: var(--my-custom-color);">
     hi!
 </div>
 </html>`;
