b00afb8b7cc3c8aaeb5da021ca47b4df8ce07576
Jim Sproull <jim@pagecloud.com>
2021-01-07 14:00:34 -0500

    handle errors

--- index.js
+++ index.js
@@ -1,45 +1,12 @@
 import express from "express";
-import { check, validationResult } from "express-validator";
+import expressValidator from "express-validator";
 
 import { processCss } from "./process.mjs";
 import { redirectSsl } from "./ssl.js";
 
-const { SECRET_KEY, PORT } = process.env;
+const { SECRET_KEY = "foo", PORT = 3002 } = process.env;
+const { check, validationResult } = expressValidator;
 
-const htmlSource = `<html>
-<head>
-<style>
-    :root {
-        --pc-color-1: rgba(0,1,2,.3);
-        --pc-color-2: red;
-    }
-    :root {
-        --my-custom-color: var(--pc-color-2);
-    }
-</style>
-<style>
-    .foo {
-        color: var(--pc-color-1);
-        background-color: var(--pc-color-2);
-        border-color: var(--my-custom-color);
-    }
-
-    ::placeholder {
-        color: gray;
-        }
-
-        @media (min-resolution: 2dppx) {
-        .image {
-            background-image: url(image@2x.png);
-        }
-        }
-</style>
-<div class="foo" style="color: var(--my-custom-color);">
-    hi!
-</div>
-</html>`;
-
-const source = htmlSource;
 const SSL_ENVIRONMENTS = ["staging", "production"];
 const app = express();
 app.use(express.json({ limit: "100mb", extended: true }));
@@ -50,9 +17,7 @@ if (
     SSL_ENVIRONMENTS.find((env) => env === process.env.NODE_ENV)
 ) {
     app.use(redirectSsl());
-    // sentry.configure(app);
 }
-// app.use(bodyParser.raw({type: 'application/*', limit: '700kb'}));
 
 app.post(
     "/",
@@ -66,7 +31,7 @@ app.post(
         }),
         check("key").equals(SECRET_KEY),
     ],
-    async (req, res) => {
+    async (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(422).json({ errors: errors.array() });
@@ -74,14 +39,13 @@ app.post(
         try {
             res.send(await processCss(req.body.html));
         } catch (e) {
-            console.log(e);
+            next(e);
         }
+        return null;
     },
 );
-// app.post('/ssr', routes.objectScreenshot);
-// app.get('/ss/:bucketName/:siteName/*', routes.pageScreenshot);
-// app.get('/*' , async (req, res) => res.status(404).end());
 
 app.listen(PORT, () => {
+    // eslint-disable-next-line no-console
     console.log(`Listening on port ${PORT}...`);
 });
